0ea99bcd354da639468fc5b64cee0871d3b4e6f0
ktjones <todd@ktjones.net>
2014-05-11 13:30:09 -0500

    3
    3

---
+++ Ch.04/Ch.04 - Ex.02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch._04___Ex._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int v1;
+            int v2;
+            bool flag = false;
+
+            do
+            {
+
+                Console.WriteLine("Enter the first Number (> than 10, Please): ");
+                v1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the first Number (> than 10, Please): ");
+                v2 = Convert.ToInt32(Console.ReadLine());
+                if (v1 <= 10 && v2 <= 10)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
+
+            }
+            while (!flag);
+
+            Console.WriteLine("You entered {0} and {1}", v1, v2);
+            Console.ReadKey(); 
+        
+        }
+    }
+}
