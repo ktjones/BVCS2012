5da0c26e6cb7a10e3051d6b696b81f0c618f69d7
ktjones <todd@ktjones.net>
2014-07-16 20:39:44 -0500

    7
    7

---
+++ Ch.10/ChlOCardLib/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChlOCardLib
+{
+    public class Card
+    {
+
+        public readonly Suit suit;
+        public readonly Rank rank;
+
+        public Card (Suit newsuit, Rank newrank)
+        {
+
+            suit = newsuit;
+            rank = newrank;
+
+        }
+
+        private Card()
+        {
+        }
+
+        
+        public override string ToString()
+        {
+            return "The " + rank + " of " + suit + "s";
+        }
+    
+    
+    
+    }
+}
---
+++ Ch.10/ChlOCardLib/Suit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChlOCardLib
+{
+
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade,
+    }
+
+}
