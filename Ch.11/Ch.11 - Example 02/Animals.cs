0c1771ef7c0d7b4e849cbd6785653c62f398b157
ktjones <todd@ktjones.net>
2014-08-02 17:14:40 -0500

    9
    9

---
+++ Ch.11/Ch.11 - Example 02/Animals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch._11___Example_02
+{
+    public class Animals : CollectionBase
+    {
+        public void Add(Animal newAnimal)
+        {
+            List.Add(newAnimal);
+        }
+        public void Remove(Animal newAnimal)
+        {
+            List.Remove(newAnimal);
+        }
+        public Animal this[int animalIndex]
+        {
+            get
+            {
+                return (Animal)List[animalIndex];
+            }
+            set
+            {
+                List[animalIndex] = value;
+            }
+        }
+    }
+}
---
+++ Ch.11/Ch11CardLib/Suit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11CardLib
+{
+
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade,
+    }
+
+}
